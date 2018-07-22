b8e3002a5dcec3bc5547bacc0729f11f601c6a87
Josh Meserve <sfrjoshpro@gmail.com>
2018-07-22 15:45:39 -0400

    Added other Page and routhig

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/Contracts', function(req, res, next) {
+  res.render('other', { title: 'Conract' ,layout: 'other'});
+});
+router.get('/Apply', function(req, res, next) {
+  res.render('other', {
+     title: 'Aplly',
+     class: 'Apply',
+     formBody:"<script type='text/javascript' defer src='//www.123formbuilder.com/embed/3975968.js' data-role='form' data-default-width='650px'></script>",
+     layout: 'other' 
+    } );
+});
+
 
 module.exports = router;
